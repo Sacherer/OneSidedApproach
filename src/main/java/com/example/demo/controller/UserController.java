3b022bd81abf31fb9c885560adb19cb51a135734
Sacher <Sacherer@163.com>
2019-07-12 21:01:03 +0800

    recode

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,22 +1,11 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 @CrossOrigin
 public class UserController {
 
-    @RequestMapping("/register")
-    public void register() {
-
-    }
-
-    @RequestMapping("/adminRegister")
-    public void AdminRegister() {
-
-    }
 
 }
